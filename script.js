8f1c424d74b51af54b989e866e0fc4e9cd338383
amandeep-rayat <118011469+amandeep-rayat@users.noreply.github.com>
2024-05-12 00:56:00 +0530

    added hints

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let cont = document.getElementById("container");
-let names = ["apple", "banana", "orange", "papaya"];
-let aname = names[Math.floor(Math.random() * (names.length - 1))];
+let names = [{ "aname": "apple", "ahint": "a red coloured famous fruit" }, { "aname": "litchi", "ahint": "a red coloured rough fruit" }, { "aname": "pineapple", "ahint": "a very rough fruit" }];
+let { aname, ahint } = names[Math.floor(Math.random() * (names.length - 1))];
 let click = document.querySelector("button");
 let chances = aname.length + 1;
 let inp = document.querySelector("input");
 let chancesleft = document.getElementById("chances");
 chancesleft.innerHTML = "Chances left: " + chances;
 let val, list;
+let hint = document.getElementById("hint");
+hint.innerHTML = "Hint: " + ahint;
+
 for (let i = 0; i < aname.length; i++) {
     cont.appendChild(document.createElement("div"));
     cont.lastChild.innerText = "_";
